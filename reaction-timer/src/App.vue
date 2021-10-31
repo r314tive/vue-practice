84d375408652135e411e9c3045790ebaaad932d7
r314tive <tanswis42@gmail.com>
2021-10-31 17:12:03 +0500

    add components to App

--- reaction-timer/src/App.vue
+++ reaction-timer/src/App.vue
@@ -1,14 +1,42 @@
 <template>
   <h1>reaction timer mini game</h1>
+  <button @click="start" :disabled="isPlaying">play</button>
+  <Block v-if="isPlaying" :delay="delay" @end="endGame"/>
+  <Results v-if="showResults" :score="score"/>
 </template>
 
 <script>
+import Block from "./components/Block.vue";
+import Results from "./components/Results.vue";
+
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-  }
-}
+    Block,
+    Results
+  },
+  data() {
+    return {
+      isPlaying: false,
+      delay: null,
+      score: null,
+      showResults: false,
+    };
+  },
+  methods: {
+    start() {
+      this.delay = 2000 + Math.random() * 5000;
+      this.isPlaying = true;
+      this.showResults = false;
+    },
+    endGame(reactionTime) {
+      this.score = reactionTime;
+      this.isPlaying = false;
+      this.showResults = true;
+    },
+  },
+};
 </script>
 
 <style>
