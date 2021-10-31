1414204cc9557ecca9fa7bae4c5f2016c8dbebdf
r314tive <tanswis42@gmail.com>
2021-10-31 17:11:18 +0500

    complete reaction-timer

---
+++ reaction-timer/src/components/Block.vue
@@ -0,0 +1,53 @@
+<template>
+  <div v-if="showBlock" class="block" @click="stopTimer">click me</div>
+</template>
+
+<script>
+export default {
+  name: "Block",
+  props: ["delay"],
+  data() {
+    return {
+      showBlock: false,
+      timer: null,
+      reactionTime: 0,
+    };
+  },
+  methods: {
+    startTimer() {
+      this.timer = setInterval(() => {
+        this.reactionTime += 10;
+      }, 10);
+    },
+    stopTimer() {
+      clearInterval(this.timer);
+      this.$emit("end", this.reactionTime);
+    },
+  },
+  mounted() {
+    // console.warn('Block component was mounted')
+    setTimeout(() => {
+      this.showBlock = true;
+      this.startTimer();
+    }, this.delay);
+  },
+  updated() {
+    // console.warn('Block component was updated')
+  },
+  unmounted() {
+    // console.warn('Block component was unmounted')
+  },
+};
+</script>
+
+<style>
+.block {
+  width: 400px;
+  border-radius: 20px;
+  background: #0faf87;
+  color: white;
+  text-align: center;
+  padding: 100px 0;
+  margin: 40px auto;
+}
+</style>
